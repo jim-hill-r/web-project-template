1e3227d66febd2b2635128fa53fc22afaa9034a6
Jim Hill <jim.hill.r@gmail.com>
2020-06-06 17:01:46 -0500

    Implemented drawer

---
+++ apps/web/src/components/drawer-content.tsx
@@ -0,0 +1,5 @@
+const DrawerContent = () => {
+    return (
+        <div> </div>
+    )
+}
--- apps/web/src/components/layout.tsx
+++ apps/web/src/components/layout.tsx
@@ -5,6 +5,7 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
+import Drawer from "@material-ui/core/Drawer"
 import IconButton from "@material-ui/core/IconButton"
 import BottomNavigation from "@material-ui/core/BottomNavigation"
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction"
@@ -27,6 +28,8 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
 const Layout = ({ children } : { children : any}) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -37,13 +40,32 @@ const Layout = ({ children } : { children : any}) => {
       }
     }
   `)
-  const classes = useStyles();
+  const classes = useStyles()
+  const [state, setState] = React.useState({
+    isDrawerOpen: false
+  });
+
+  const toggleDrawer = 
+    (open: boolean) => 
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, isDrawerOpen: open });
+    }
+
+  var anchor: Anchor = "left"
 
   return (
     <>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit">
@@ -51,6 +73,11 @@ const Layout = ({ children } : { children : any}) => {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Drawer anchor={anchor} open={state.isDrawerOpen} onClose={toggleDrawer(false)} >
+        <div onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)} >
+          Stuff goes here
+        </div>
+      </Drawer>
       <div
         style={{
           margin: `0 auto`,
