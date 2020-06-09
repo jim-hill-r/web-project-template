f9b10f8329842122a12788210efca9504c10fca3
Jim Hill <jim.hill.r@gmail.com>
2020-06-08 19:56:19 -0500

    Updated layout to optimize bottom navigation

--- apps/web/src/components/layout.tsx
+++ apps/web/src/components/layout.tsx
@@ -1,25 +1,16 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql, navigate, Link } from "gatsby"
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
-import { useMediaQuery, Typography } from "@material-ui/core"
-import { AppBar, Toolbar, Drawer, IconButton, BottomNavigation, BottomNavigationAction } from "@material-ui/core"
-import HomeIcon from "@material-ui/icons/Home"
-import RestoreIcon from "@material-ui/icons/Restore"
-import FavoriteIcon from "@material-ui/icons/Favorite"
-import LocationOnIcon from "@material-ui/icons/LocationOn"
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz"
-import MenuIcon from "@material-ui/icons/Menu"
-import "./layout.css"
-
-const navItems = [
-  { label: "Primary", icon: "<HomeIcon />", route:  "/primary" },
-  { label: "Secondary", icon: "<FavoriteIcon />", route: "/secondary" },
-  { label: "Tertiary", icon: "<LocationOnIcon />", route: "/tertiary" },
-  { label: "Quarternary", icon: "<RestoreIcon />", route: "/quarternary" },
-  { label: "Overflow One", route: "/overflow/one" },
-  { label: "Overflow Two", route: "/overflow/two" }
-]
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql, navigate, Link } from 'gatsby'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import { useMediaQuery, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Drawer, IconButton, BottomNavigation, BottomNavigationAction } from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home'
+import RestoreIcon from '@material-ui/icons/Restore'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import MenuIcon from '@material-ui/icons/Menu'
+import './layout.css'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -49,9 +40,26 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+type NavItem = {
+  label: string
+  icon: JSX.Element
+  route: string
+}
+
+const navItems : NavItem[] = [
+  { label: 'Primary', icon: <HomeIcon />, route:  '/primary' },
+  { label: 'Secondary', icon: <FavoriteIcon />, route: '/secondary' },
+  { label: 'Tertiary', icon: <LocationOnIcon />, route: '/tertiary' },
+  { label: 'Quarternary', icon: <RestoreIcon />, route: '/quarternary' },
+  { label: 'Overflow One', icon: <></>, route: '/overflow/one' },
+  { label: 'Overflow Two', icon: <></>, route: '/overflow/two' },
+]
+const bottomNavItems = navItems.slice(0,4).map(item => {
+  return <BottomNavigationAction value={item} label={item.label} icon={item.icon} />
+})
+const moreNavItem: NavItem = { label: 'More', icon: <MoreHorizIcon />, route:  '' }
+
 const Layout = ({ children } : { children : any}) => {
-  const isDesktop = useMediaQuery('(min-width:600px)')
-  const useBottomNavigation = !isDesktop
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -61,7 +69,12 @@ const Layout = ({ children } : { children : any}) => {
       }
     }
   `)
+  
   const classes = useStyles()
+
+  const useBottomNavigation = !useMediaQuery('(min-width:600px)')
+
+  const [selectedNavItem, setSelectedNavItem] = React.useState({label: ''})
   const [state, setState] = React.useState({
     isDrawerOpen: false,
     selectedNavItem: 0
@@ -83,19 +96,18 @@ const Layout = ({ children } : { children : any}) => {
 
   return (
     <>
-      <div className={useBottomNavigation ? classes.hidden : ''}>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit">
-              <Link to="/" className={classes.title}> {data.site.siteMetadata.title} </Link>
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-      <Drawer anchor={useBottomNavigation ? "bottom" : "left"} open={state.isDrawerOpen} onClose={toggleDrawer(false)} >
+      <AppBar position='static' className={useBottomNavigation ? classes.hidden : ''}>
+        <Toolbar variant='dense'>
+          <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu' onClick={toggleDrawer(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' color='inherit'>
+            <Link to='/' className={classes.title}> {data.site.siteMetadata.title} </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer anchor={useBottomNavigation ? 'bottom' : 'left'} open={state.isDrawerOpen} onClose={toggleDrawer(false)} >
         <div onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)} >
           © {new Date().getFullYear()}
         </div>
@@ -107,22 +119,21 @@ const Layout = ({ children } : { children : any}) => {
 
       <footer>
         <BottomNavigation
-          value={state.selectedNavItem}
-          onChange={(event, newValue: number) => {
-            if(newValue < 4) {
-              navigate(navItems[newValue].route)
-            } else {
+          value={selectedNavItem}
+          onChange={(event, newValue: NavItem) => {
+            console.log(event)
+            console.log(newValue)
+            if(newValue.label === moreNavItem.label) {
               setState({ ...state, isDrawerOpen: true });
+            } else {
+              setSelectedNavItem(newValue)
+              navigate(newValue.route)
             }
           }}
-          showLabels
           className={useBottomNavigation ? classes.stickToBottom : classes.hidden}
         >
-          <BottomNavigationAction label={navItems[0].label} icon={<HomeIcon />} />
-          <BottomNavigationAction label={navItems[1].label} icon={<FavoriteIcon />} />
-          <BottomNavigationAction label={navItems[2].label} icon={<LocationOnIcon />} />
-          <BottomNavigationAction label={navItems[3].label} icon={<RestoreIcon />} />
-          <BottomNavigationAction label="More" icon={<MoreHorizIcon />} />
+          {bottomNavItems}
+          <BottomNavigationAction value={moreNavItem} label={moreNavItem.label} icon={moreNavItem.icon} />
         </BottomNavigation>
       </footer>
     </>
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,15 +2,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class IndexRoute extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <SEO title="Home" />
-        <div> TODO: Landing content </div>
-      </Layout>
-    )
-  }
+const IndexRoute = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div> TODO: Landing content </div>
+    </Layout>
+  )
 }
 
 export default IndexRoute
