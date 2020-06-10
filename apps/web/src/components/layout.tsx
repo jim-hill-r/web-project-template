0eda55855728b424becc1e98df2f32ba49a01f66
Jim Hill <jim.hill.r@gmail.com>
2020-06-09 20:57:11 -0500

    Finalized navigation

--- apps/web/src/components/layout.tsx
+++ apps/web/src/components/layout.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql, navigate, Link } from 'gatsby'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import { useMediaQuery, Typography } from '@material-ui/core'
 import { AppBar, Toolbar, Drawer, IconButton, BottomNavigation, BottomNavigationAction } from '@material-ui/core'
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home'
-import RestoreIcon from '@material-ui/icons/Restore'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import LocationOnIcon from '@material-ui/icons/LocationOn'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
@@ -24,14 +28,23 @@ const useStyles = makeStyles((theme: Theme) =>
       textDecoration: 'none',
       color: 'white'
     },
-    stickToBottom: {
-      width: '100%',
+    drawerItem: {
+      paddingRight: '50px'
+    },
+    bottomNavStyle: {
       position: 'fixed',
-      bottom: 0,
+      left: '0px',
+      bottom: '0px',
+      height: '50px',
+      width: '100%',
+      zIndex: 100,
+      '&:selected': {
+         color: "#00bcd4"
+      },
     },
     main: {
       margin: `0 auto`,
-      maxWidth: 960,
+      maxWidth: 600,
       padding: `0 1.0875rem 1.45rem`,
     },
     hidden: {
@@ -50,12 +63,12 @@ const navItems : NavItem[] = [
   { label: 'Primary', icon: <HomeIcon />, route:  '/primary' },
   { label: 'Secondary', icon: <FavoriteIcon />, route: '/secondary' },
   { label: 'Tertiary', icon: <LocationOnIcon />, route: '/tertiary' },
-  { label: 'Quarternary', icon: <RestoreIcon />, route: '/quarternary' },
+  { label: 'Quarternary', icon: <></>, route: '/quarternary' },
   { label: 'Overflow One', icon: <></>, route: '/overflow/one' },
   { label: 'Overflow Two', icon: <></>, route: '/overflow/two' },
 ]
-const bottomNavItems = navItems.slice(0,4).map(item => {
-  return <BottomNavigationAction value={item} label={item.label} icon={item.icon} />
+const bottomNavItems = navItems.slice(0,3).map(item => {
+  return <BottomNavigationAction key={item.label} value={item} label={item.label} icon={item.icon} />
 })
 const moreNavItem: NavItem = { label: 'More', icon: <MoreHorizIcon />, route:  '' }
 
@@ -75,6 +88,13 @@ const Layout = ({ children } : { children : any}) => {
   const useBottomNavigation = !useMediaQuery('(min-width:600px)')
 
   const [selectedNavItem, setSelectedNavItem] = React.useState({label: ''})
+  useEffect(() => {
+    var currentNavItem = navItems.find((item) => item.route === location.pathname)
+    if(currentNavItem != null) {
+      setSelectedNavItem(currentNavItem)
+    }
+  });
+
   const [state, setState] = React.useState({
     isDrawerOpen: false,
     selectedNavItem: 0
@@ -109,6 +129,23 @@ const Layout = ({ children } : { children : any}) => {
 
       <Drawer anchor={useBottomNavigation ? 'bottom' : 'left'} open={state.isDrawerOpen} onClose={toggleDrawer(false)} >
         <div onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)} >
+        <List>
+          {navItems.slice(0,3).map((item) => (
+            <ListItem className={classes.drawerItem} button key={item.label}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.label} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {navItems.slice(3).map((item) => (
+            <ListItem className={classes.drawerItem} button key={item.label}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.label} />
+            </ListItem>
+          ))}
+        </List>
           © {new Date().getFullYear()}
         </div>
       </Drawer>
@@ -121,8 +158,6 @@ const Layout = ({ children } : { children : any}) => {
         <BottomNavigation
           value={selectedNavItem}
           onChange={(event, newValue: NavItem) => {
-            console.log(event)
-            console.log(newValue)
             if(newValue.label === moreNavItem.label) {
               setState({ ...state, isDrawerOpen: true });
             } else {
@@ -130,9 +165,11 @@ const Layout = ({ children } : { children : any}) => {
               navigate(newValue.route)
             }
           }}
-          className={useBottomNavigation ? classes.stickToBottom : classes.hidden}
+          className={useBottomNavigation ? classes.bottomNavStyle : classes.hidden}
         >
-          {bottomNavItems}
+          {navItems.slice(0,3).map(item => {
+            return <BottomNavigationAction key={item.label} value={item} label={item.label} icon={item.icon} />
+          })}
           <BottomNavigationAction value={moreNavItem} label={moreNavItem.label} icon={moreNavItem.icon} />
         </BottomNavigation>
       </footer>
